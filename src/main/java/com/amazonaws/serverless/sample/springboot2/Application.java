a1675397f968d69541cabc9389402d67965d1940
Michael Lifshits <michael.lifschitz@gmail.com>
2019-10-10 18:10:07 -0400

    cleanup

--- src/main/java/com/amazonaws/serverless/sample/springboot2/Application.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot2/Application.java
@@ -1,26 +1,41 @@
 package com.amazonaws.serverless.sample.springboot2;
 
 import com.amazonaws.serverless.sample.springboot2.controller.PetsController;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.reactor.core.ReactorCoreAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-
-@SpringBootApplication
-@Import({ PetsController.class })
+@SpringBootConfiguration
+@Import({
+        ConfigurationPropertiesAutoConfiguration.class,
+        PropertyPlaceholderAutoConfiguration.class,
+        ServletWebServerFactoryAutoConfiguration.class,
+        PetsController.class,
+        JacksonAutoConfiguration.class,
+        WebMvcAutoConfiguration.class,
+        ReactorCoreAutoConfiguration.class,
+        RestTemplateAutoConfiguration.class,
+        EmbeddedWebServerFactoryCustomizerAutoConfiguration.class,
+        DispatcherServletAutoConfiguration.class,
+        HttpEncodingAutoConfiguration.class
+})
 public class Application {
 
     // silence console logging
